bd50e57500c4ac7429580f99aac0a66ff4776642
kalitusboy <kalitusboy2019@gmail.com>
2026-05-03 23:55:49 +0100

    Delete android/app/src/main/kotlin/com/ihsa directory

--- android/app/src/main/kotlin/com/ihsa/nassim2026_v8/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ihsa.nassim2026_v8
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/ihsa/rural_nassim/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.ihsa.rural_nassim
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
